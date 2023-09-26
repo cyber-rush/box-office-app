0b391ffc2ece90f1c2259a78aa6e4e7e26609f3a
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2023-09-26 17:30:35 +0530

    workibg with react-query to fetch data replacing the useEffect and custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/starred" element={<Starred />}></Route>
-        </Route>
-        <Route path="/show/:showId" element={<Show />} />
-        <Route path="*" element={<div>Page Not Found!!!</div>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/starred" element={<Starred />}></Route>
+          </Route>
+          <Route path="/show/:showId" element={<Show />} />
+          <Route path="*" element={<div>Page Not Found!!!</div>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+
   );
 }
 
