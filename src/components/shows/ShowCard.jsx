19827664c8ac3f279cb43af14680d15a7bb005a5
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2023-09-27 11:59:26 +0530

    adding go back button on home page

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 
 const ShowCard = ({ name, image, summary, id }) => {
     // split the summary into an array , take first 10 elements, then join those 10 elements , and then replace the html tags
@@ -10,7 +9,7 @@ const ShowCard = ({ name, image, summary, id }) => {
         <h1 >{name}</h1>
         <p>{summaryStripped}</p>
         <div>
-            <Link to={`show/${id}`}>Read More</Link>
+            <a href={`show/${id}`} target="_blank" rel="noreferrer">Read More</a>
             <button type="button">Star me!!!</button>
         </div>
     </div>
